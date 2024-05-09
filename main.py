3b1d29fbbb49124f9a1ba398b9e5b76cfc1ef181
Chase Leibowitz <cleibowitz@shipleyschool.org>
2024-05-08 23:21:14 -0400

    Initial commit and push

---
+++ main.py
@@ -0,0 +1,63 @@
+"""
+Chase Leibowitz & Jacob Ominsky
+ML IS 2023-24 Final Project
+
+Objective: Use LSTM network to predict stock prices using GOOGL stock data
+"""
+
+# import libraries
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#%matplotlib inline
+from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow
+import keras
+from keras import models
+from keras import layers
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Dropout
+
+# initialize full dataset
+dataset_train = pd.read_csv("GOOGL.csv")
+
+# pick training set
+training_set = dataset_train.iloc[:,1:2].values
+
+# normalize training set
+scaler = MinMaxScaler(feature_range = (0,1))
+scaled_training_set = scaler.fit_transform(training_set)
+
+# creating X_train and Y_train data structures
+X_train = []
+Y_train = []
+for i in range (60, 1258):
+    X_train.append(scaled_training_set[i-60:i, 0])
+    Y_train.append(scaled_training_set[i, 0])
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+# build the model by adding the layers to the LSTM
+regressor = Sequential()
+regressor.add(LSTM(units = 50, return_sequences= True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50, return_sequences=True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units=50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units=1))
+
+# fitting the moddle
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+regressor.fit(X_train, Y_train, epochs=100, batch_size=32)
+
